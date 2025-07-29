3211f0d6b2c5721f5955f577a646033cb83bb0e9
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-30 01:19:44 +0600

    fix: correct router variable name and define login and refresh-token routes

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { authController } from "./auth.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { userLoginJodValidation } from "./auth.validation";
 
-const routes = Router();
+const router = Router();
 
-export const authRoutes = routes;
+router.post("/login", validateRequest(userLoginJodValidation), authController.credentialLogin);
+router.post("/refresh-token", authController.generateAccessTokenFromRefreshToken);
+export const authRoutes = router;
