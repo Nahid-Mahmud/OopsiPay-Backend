91b58bcac2795717be99fc55663cfe1ae5283f70
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-30 15:21:36 +0600

    feat: implement send OTP email functionality with Redis storage

---
+++ src/app/utils/sendOtpEmail.ts
@@ -0,0 +1,36 @@
+import { StatusCodes } from "http-status-codes";
+import AppError from "../errorHelpers/AppError";
+import User from "../modules/user/user.model";
+import { generateOtp } from "./generateOtp";
+import { redisClient } from "../config/redis.config";
+import { sendEmail } from "./sendEmail";
+
+export const sendOtpEmail = async ({ email, expirationTimeInSeconds }: { email: string; expirationTimeInSeconds: number }) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  if (user.isVerified) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "User is already verified");
+  }
+
+  const otp = generateOtp(6);
+  // store otp in redis
+  const redisKey = `otp:${email}`;
+  await redisClient.set(redisKey, otp, {
+    expiration: {
+      type: "EX",
+      value: expirationTimeInSeconds,
+    },
+  });
+
+  await sendEmail({
+    to: email,
+    subject: "Your OTP Code",
+    templateName: "otp.ejs",
+    templateData: {
+      name: user.firstName + " " + user.lastName,
+      otp: otp,
+    },
+  });
+};
