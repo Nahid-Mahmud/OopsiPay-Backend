2159ba21b4f9c0dcddc396db6060e7ca75057350
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-30 15:34:13 +0600

    feat: remove sendOtp function and related OTP generation logic

--- src/app/modules/otp/otp.service.ts
+++ src/app/modules/otp/otp.service.ts
@@ -1,42 +1,7 @@
 import { StatusCodes } from "http-status-codes";
-import { redisClient } from "../../config/redis.config";
 import AppError from "../../errorHelpers/AppError";
-import { generateOtp } from "../../utils/generateOtp";
-import { sendEmail } from "../../utils/sendEmail";
-import User from "../user/user.model";
 import verifyOtp from "../../utils/verifyOtp";
-
-const OTP_EXPIRATION_TIME = 2 * 60; // 2 minutes in seconds
-
-const sendOtp = async (email: string) => {
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
-  }
-  if (user.isVerified) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "User is already verified");
-  }
-
-  const otp = generateOtp(6);
-  // store otp in redis
-  const redisKey = `otp:${email}`;
-  await redisClient.set(redisKey, otp, {
-    expiration: {
-      type: "EX",
-      value: OTP_EXPIRATION_TIME,
-    },
-  });
-
-  await sendEmail({
-    to: email,
-    subject: "Your OTP Code",
-    templateName: "otp.ejs",
-    templateData: {
-      name: user.firstName + " " + user.lastName,
-      otp: otp,
-    },
-  });
-};
+import User from "../user/user.model";
 
 const verifyOtpUser = async (email: string, otp: string) => {
   const session = await User.startSession();
@@ -61,6 +26,5 @@ const verifyOtpUser = async (email: string, otp: string) => {
 };
 
 export const otpServices = {
-  sendOtp,
   verifyOtpUser,
 };
