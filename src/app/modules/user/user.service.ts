4dc9c75f6c1a01be3e047590b7b63ee661c895d5
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-30 20:32:25 +0600

    feat: implement updateUser function with role-based access control and validation

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,9 +2,11 @@ import { StatusCodes } from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
 import { sendOtpEmail } from "../../utils/sendOtpEmail";
 import { hashPassword } from "./../../utils/hashPassword";
-import { IUser } from "./user.interface";
+import { IUser, UserRole } from "./user.interface";
 import User from "./user.model";
+import { JwtPayload } from "jsonwebtoken";
 
+// create user
 const createUser = async (payload: Partial<IUser>) => {
   const { password, email } = payload;
 
@@ -56,6 +58,78 @@ const createUser = async (payload: Partial<IUser>) => {
   }
 };
 
+// update user
+
+const updateUser = async (userId: string, payload: Partial<IUser>, decodedToken: JwtPayload) => {
+  if (decodedToken.role === UserRole.USER || decodedToken.role === UserRole.AGENT) {
+    if (userId !== decodedToken.userId) {
+      throw new AppError(StatusCodes.FORBIDDEN, "You do not have permission to update this user");
+    }
+  }
+
+  // check if user exists
+
+  const user = await User.findById(decodedToken.userId);
+
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+
+  // role update validation
+
+  if (decodedToken.role === UserRole.ADMIN && user.role === UserRole.SUPER_ADMIN) {
+    throw new AppError(StatusCodes.FORBIDDEN, "You do not have permission to update this user");
+  }
+
+  if (payload.role) {
+    if (decodedToken.role === UserRole.USER || decodedToken.role === UserRole.AGENT) {
+      throw new AppError(StatusCodes.FORBIDDEN, "You do not have permission to change user role");
+    }
+  }
+
+  // isActive, isDeleted, isVerified update validation
+  if (payload.isActive || payload.isDeleted || payload.isVerified) {
+    if (decodedToken.role === UserRole.USER || decodedToken.role === UserRole.AGENT) {
+      throw new AppError(StatusCodes.FORBIDDEN, "You do not have permission to change user status");
+    }
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(userId, payload, {
+    new: true,
+    runValidators: true,
+  }).select("-password");
+  return updatedUser;
+};
+
+// get all users
+const getAllUsers = async () => {
+  const users = await User.find({}).select("-password");
+  const totalUsers = await User.countDocuments();
+  return { data: users, meta: totalUsers };
+};
+
+// get logged-in user
+const getMe = async (userId: string) => {
+  const user = await User.findById(userId).select("-password");
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  return user;
+};
+
+// get user by userID
+const getUserById = async (userId: string) => {
+  const user = await User.findById(userId).select("-password");
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  return user;
+};
+
 export const userService = {
   createUser,
+  updateUser,
+  getAllUsers,
+  getUserById,
+  getMe,
 };
