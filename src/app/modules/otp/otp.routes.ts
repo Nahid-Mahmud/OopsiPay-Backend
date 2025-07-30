efc8a3d49b3de659821635d7a4d4a3c103b46706
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-30 15:34:06 +0600

    feat: update OTP route to change endpoint from /send to /resend

--- src/app/modules/otp/otp.routes.ts
+++ src/app/modules/otp/otp.routes.ts
@@ -3,7 +3,7 @@ import { otpController } from "./otp.controller";
 
 const router = Router();
 
-router.post("/send", otpController.sendOtp);
+router.post("/resend", otpController.sendOtp);
 
 router.post("/verify-user", otpController.verifyOtpUser);
 
