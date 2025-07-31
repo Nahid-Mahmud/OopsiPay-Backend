57e78bbae2fdc62b1e1eaa08ccf291c10940c1e0
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-31 22:27:44 +0600

    feat: add route to get wallet by userId with admin and super admin authentication

--- src/app/modules/wallet/wallet.routes.ts
+++ src/app/modules/wallet/wallet.routes.ts
@@ -8,5 +8,7 @@ const router = Router();
 router.patch("/type/:walletId", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), walletController.updateWalletType);
 router.get("/me", checkAuth(...Object.values(UserRole)), walletController.getMyWallet);
 router.get("/get-all", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), walletController.getAllWallets);
+//  get wallet by userId
+router.get("/:userId", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), walletController.getWalletByUserId);
 
 export const walletRoutes = router;
