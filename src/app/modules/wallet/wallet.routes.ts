d5e844eba3d63ae8743745a828066ef0c7060c07
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-25 04:41:24 +0600

    feat: add updateWalletStatus endpoint with validation for wallet status updates

--- src/app/modules/wallet/wallet.routes.ts
+++ src/app/modules/wallet/wallet.routes.ts
@@ -2,12 +2,21 @@ import { Router } from "express";
 import { walletController } from "./wallet.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { UserRole } from "../user/user.interface";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { updateWalletStatusZodValidation } from "./wallet.validation";
 
 const router = Router();
 
 router.patch("/type/:walletId", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), walletController.updateWalletType);
 router.get("/me", checkAuth(...Object.values(UserRole)), walletController.getMyWallet);
 router.get("/get-all", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), walletController.getAllWallets);
+
+router.patch(
+  "/update-wallet-status/:walletId",
+  validateRequest(updateWalletStatusZodValidation),
+  checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  walletController.updateWalletStatus
+);
 //  get wallet by userId
 router.get("/:userId", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), walletController.getWalletByUserId);
 
