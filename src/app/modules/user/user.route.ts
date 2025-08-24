4d7c50da0035c48f6cc58d786e54dc3eaba63b50
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-25 04:41:07 +0600

    refactor: reorganize user route handlers and update change-pin endpoint to use PATCH method

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,24 +1,40 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
-import { validateRequest } from "../../middlewares/validateRequest";
-import { updateUserZodSchema, userCreateZodSchema } from "./user.validation";
-import { checkAuth } from "../../middlewares/checkAuth";
-import { UserRole } from "./user.interface";
+
 import { multerUpload } from "../../config/multer.config";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { UserRole } from "./user.interface";
+import {
+  changePinZodSchema,
+  setPinFirstTimeZodSchema,
+  updateUserZodSchema,
+  userCreateZodSchema,
+} from "./user.validation";
 
 const router = Router();
 
 // create user
 router.post("/create", validateRequest(userCreateZodSchema), userController.createUser);
 
-router.patch("/set-pin", checkAuth(...Object.values(UserRole)), userController.setPinFirstTime);
+router.patch(
+  "/set-pin",
+  validateRequest(setPinFirstTimeZodSchema),
+  checkAuth(...Object.values(UserRole)),
+  userController.setPinFirstTime
+);
 
 // get All Users
 router.get("/get-all", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), userController.getAllUsers);
 
 // get me route
 router.get("/me", checkAuth(...Object.values(UserRole)), userController.getMe);
-router.post("/change-pin", checkAuth(...Object.values(UserRole)), userController.changePin);
+router.patch(
+  "/change-pin",
+  validateRequest(changePinZodSchema),
+  checkAuth(...Object.values(UserRole)),
+  userController.changePin
+);
 // update user
 router.patch(
   "/:userId",
