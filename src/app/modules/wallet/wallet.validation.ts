9dbdb31764c599b7704396e27d2ddcb99326f177
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-25 04:41:32 +0600

    feat: add validation for wallet status updates in wallet.validation.ts

--- src/app/modules/wallet/wallet.validation.ts
+++ src/app/modules/wallet/wallet.validation.ts
@@ -1,6 +1,10 @@
 import z from "zod";
-import { WalletType } from "./wallet.interface";
+import { WalletStatus, WalletType } from "./wallet.interface";
 
 export const updateWalletTypeZodValidation = z.object({
   walletType: z.enum(Object.values(WalletType)).optional(),
+});
+
+export const updateWalletStatusZodValidation = z.object({
+  walletStatus: z.enum(Object.values(WalletStatus)).optional(),
 });
